edab840605e08fd87b5220192b6246ed248753f0
Zorro909 <an@maximilian-kling.de>
2022-02-07 20:22:10 +0100

    Replaced transient with @JsonIgnore

--- event-system/src/main/java/de/zorro909/blank/event/BlankRpcEvent.java
+++ event-system/src/main/java/de/zorro909/blank/event/BlankRpcEvent.java
@@ -1,13 +1,17 @@
 package de.zorro909.blank.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 
 public abstract class BlankRpcEvent<R> extends BlankEvent {
 
+    @JsonIgnore
     @Getter
-    private transient Class<R> responseType;
+    private Class<R> responseType;
     
-    private transient R response;
+    @JsonIgnore
+    private R response;
     
     protected BlankRpcEvent(Class<R> responseType) {
 	this.responseType = responseType;
