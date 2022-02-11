9290f12e7c2cf8c48adc28be7699b7216f09c016
Zorro909 <an@maximilian-kling.de>
2022-02-11 17:45:56 +0100

    Edited Deployment Scripts

--- dockerBuild/releaseDockerVersion.sh
+++ dockerBuild/releaseDockerVersion.sh
@@ -5,11 +5,9 @@ mvn clean install
 cp blankdiscordbot-core/target/blankdiscordbot-core-*.jar dockerBuild/BlankDiscordBot.jar
 cp blankdiscordbot-core/src/main/resources/application.yml dockerBuild/
 
-cd blankdiscordbot-core
-
 export TAG=$(echo '${project.version}' | mvn help:evaluate | grep -v '^[[]')
 
-cd ../dockerBuild
+cd dockerBuild
 
 docker build -t zorro909/blankdiscordbot:$TAG .
 docker push zorro909/blankdiscordbot:$TAG
